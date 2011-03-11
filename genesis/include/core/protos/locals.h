cdf2006bbebad81b24a418ccd19f9fac7aace31a
Mike Austin <mike_ekim@yahoo.com>
2011-03-10 23:49:52 -0800

    Missing file

---
+++ genesis/include/core/protos/locals.h
@@ -0,0 +1,29 @@
+//
+// core/protos/locals.h
+//
+// Copyright 2008-2011 Mike Austin
+// All rights reserved.
+//
+
+#ifndef IMPULSE_LOCALS_H
+#define IMPULSE_LOCALS_H
+
+#include "core/frame.h"
+#include "core/array.h"
+
+namespace impulse {
+
+ //
+ // class LocalsProto
+ //
+
+	class LocalsProto : public Frame {
+
+	 public:
+
+	};
+
+}
+
+#endif
+
