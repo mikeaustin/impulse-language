3ae549ed9868a336d82fe1e50e34eb5fd66cb213
Mike Austin <mike@mike-austin.com>
2011-12-27 13:59:10 -0800

    Bug fixes and test cases

---
+++ ruby2/core/protos/boolean.rb
@@ -0,0 +1,38 @@
+#
+# core/protos/boolean.rb
+#
+
+
+class BooleanProto < Frame
+
+  def self.instance
+    @instance ||= BooleanProto.new(ObjectProto.instance)
+
+    @instance.add_method2(:"==", [@instance])   { |receiver, args| Value(receiver.float == args[0].float) }
+    @instance.add_method2(:"!=", [@instance])   { |receiver, args| Value(receiver.float != args[0].float) }
+    @instance.add_method2(:"not", [])  { |receiver, args| Value(!receiver.float) }
+    
+    return @instance
+  end
+
+  def frame_inspect(value)
+    if value.frame == BooleanProto.instance.frame
+      return "<bool>"
+    else
+      return value.float.to_s
+    end
+  end
+
+end
+
+
+class BooleanValue < BooleanProto
+
+  def self.instance
+    @instance = BooleanValue.new(BooleanProto.instance)
+    
+    return @instance
+  end
+
+end
+
